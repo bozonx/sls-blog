4aa78de18d07ff8d042092edc4adfcc0c6fdee6d
Ivan <ipkozyrin@gmail.com>
2025-09-10 08:48:20 -0300

    change

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -7,36 +7,38 @@ const ru = loadBlogLocale("ru", __filename, PROPS);
 const en = loadBlogLocale("en", __filename, PROPS);
 const configBase = blogConfigBase(PROPS, en);
 
-export default defineConfig({
-  ...configBase,
-  locales: {
-    ...configBase.locales,
-    en: { lang: "en-US", ...en },
-    ru: { lang: "ru-RU", ...ru },
-  },
-  themeConfig: {
-    ...configBase.themeConfig,
-    showAuthorInPostList: false,
-    homeBgParalaxOffset: 150,
-    sidebarLogoSrc: "/img/sidebar-logo.webp",
-  },
-  head: [
-    ...configBase.head,
-    // do not recognize telephone numbers on the page
-    ["meta", { name: "format-detection", content: "telephone=no" }],
+export default async () => {
+  return defineConfig({
+    ...configBase,
+    locales: {
+      ...configBase.locales,
+      en: { lang: "en-US", ...en },
+      ru: { lang: "ru-RU", ...ru },
+    },
+    themeConfig: {
+      ...configBase.themeConfig,
+      showAuthorInPostList: false,
+      homeBgParalaxOffset: 150,
+      sidebarLogoSrc: "/img/sidebar-logo.webp",
+    },
+    head: [
+      ...configBase.head,
+      // do not recognize telephone numbers on the page
+      ["meta", { name: "format-detection", content: "telephone=no" }],
 
-    [
-      "script",
-      {
-        async: "",
-        src: "https://www.googletagmanager.com/gtag/js?id=G-B9R514ZW75",
-      },
-    ],
-    [
-      "script",
-      {},
-      `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());
+      [
+        "script",
+        {
+          async: "",
+          src: "https://www.googletagmanager.com/gtag/js?id=G-B9R514ZW75",
+        },
+      ],
+      [
+        "script",
+        {},
+        `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());
        gtag('config', 'G-B9R514ZW75');`,
+      ],
     ],
-  ],
-});
+  });
+};
--- src/en/loadPosts.data.js
+++ src/en/loadPosts.data.js
@@ -1,11 +1,14 @@
-import { makePreviewItem } from "vitepress-sls-blog-tmpl/makePreviewItem.js";
 import { POSTS_DIR } from "vitepress-sls-blog-tmpl/constants.js";
+import getCachedPosts from "./cachedPosts.js";
 
 export default {
   watch: [`./${POSTS_DIR}/*.md`],
-  load(watchedFiles) {
+  async load(watchedFiles) {
     return {
-      posts: watchedFiles.map((item) => makePreviewItem(item)),
+      posts: await getCachedPosts(
+        watchedFiles,
+        process.env.NODE_ENV !== "production"
+      ),
     };
   },
 };
