12104ac70cdba4b92962dcca0a9e8dadcff5ca91
Ivan <ipkozyrin@gmail.com>
2025-09-10 10:06:50 -0300

    add

--- src/en/cachedPosts.js
+++ src/en/cachedPosts.js
@@ -4,7 +4,5 @@ import { loadPosts } from "vitepress-sls-blog-tmpl/loadPosts.js";
 const localeDir = path.dirname(import.meta.url.replace("file://", ""));
 
 export default async function (ignoreCache = false) {
-  const posts = await loadPosts(localeDir, ignoreCache);
-
-  return posts;
+  return loadPosts(localeDir, ignoreCache);
 }
