9d8518f64595824fc4141463c6f754c21fcf5784
Ivan <ipkozyrin@gmail.com>
2025-09-16 12:14:02 -0300

    change

--- src/.vitepress/theme/Layout.vue
+++ src/.vitepress/theme/Layout.vue
@@ -2,15 +2,18 @@
   import Theme from "vitepress-sls-blog-tmpl";
   import { data as ruData } from "../../ru/loadPosts.data.js";
   import { data as enData } from "../../en/loadPosts.data.js";
+  import { provide } from "vue";
 
   const posts = {
     ru: ruData.posts,
     en: enData.posts,
   };
 
+  provide("posts", posts);
+
   const { Layout } = Theme;
 </script>
 
 <template>
-  <Layout :posts="posts"></Layout>
+  <Layout></Layout>
 </template>
--- src/.vitepress/theme/index.js
+++ src/.vitepress/theme/index.js
@@ -1,12 +1,10 @@
 import Layout from "./Layout.vue";
-import { YouTubeEmbed } from "@miletorix/vitepress-youtube-embed";
+import Theme from "vitepress-sls-blog-tmpl";
 import "@miletorix/vitepress-youtube-embed/style.css";
 import "vitepress-sls-blog-tmpl/styles.css";
 import "./styles.css";
 
 export default {
   Layout,
-  enhanceApp(ctx) {
-    ctx.app.component("YouTubeEmbed", YouTubeEmbed);
-  },
+  extends: Theme,
 };
