31b764ac277f9b61a8140305d9cadd194f2f281f
Ivan <ipkozyrin@gmail.com>
2025-09-18 10:41:50 -0300

    add

--- src/.vitepress/props.js
+++ src/.vitepress/props.js
@@ -2,6 +2,5 @@ export const PROPS = {
   hostname: "https://blog.p-libereco.org",
   repo: "https://github.com/bozonx/sls-blog",
   siteUrl: "https://p-libereco.org",
-  footerCopyright: "Copyright © 2022-present The system of personal freedom.",
   perPage: 20,
 };
