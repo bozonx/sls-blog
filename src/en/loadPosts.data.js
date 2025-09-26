da91453679dcb630fd44d1719694d8b55f5cf844
Ivan <ipkozyrin@gmail.com>
2025-09-26 18:54:01 -0300

    fix

--- src/en/loadPosts.data.js
+++ src/en/loadPosts.data.js
@@ -1,11 +1,17 @@
+import path from "path";
 import { POSTS_DIR } from "vitepress-sls-blog-tmpl/constants.js";
-import getAllPosts from "./getAllPosts.js";
+import { loadPostsData } from "vitepress-sls-blog-tmpl/loadPosts.js";
+
+const config = globalThis.VITEPRESS_CONFIG;
+const localeDir = path.dirname(import.meta.url.replace("file://", ""));
 
 export default {
   watch: [`./${POSTS_DIR}/*.md`],
   async load(watchedFiles) {
     return {
-      posts: await getAllPosts(
+      posts: await loadPostsData(
+        localeDir,
+        config,
         watchedFiles,
         process.env.NODE_ENV !== "production"
       ),
