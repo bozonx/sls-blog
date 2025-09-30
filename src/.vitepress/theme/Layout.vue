5dbc0b540859ae9ee540ae04d024abfe23d92b0f
Ivan <ipkozyrin@gmail.com>
2025-09-30 13:14:07 -0300

    fix

--- src/.vitepress/theme/Layout.vue
+++ src/.vitepress/theme/Layout.vue
@@ -3,21 +3,13 @@
   import { provide } from "vue";
   import { data as ruData } from "../../ru/loadPosts.data.js";
   import { data as enData } from "../../en/loadPosts.data.js";
-  import { data as ruPopularPostsData } from "../../ru/loadPopularPosts.data.js";
-  import { data as enPopularPostsData } from "../../en/loadPopularPosts.data.js";
 
   const posts = {
     ru: ruData.posts,
     en: enData.posts,
   };
 
-  const popularPosts = {
-    ru: ruPopularPostsData.posts,
-    en: enPopularPostsData.posts,
-  };
-
   provide("posts", posts);
-  provide("popularPosts", popularPosts);
 
   const { Layout } = Theme;
 </script>
